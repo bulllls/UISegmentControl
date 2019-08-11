94c4bf83d5770a3428a7bb18855142d21841ed6d
Ruslan <ruslanfilistovich@gmail.com>
2019-08-11 13:15:22 +0300

    UITextField: add UIAlertController with UIAlertAction

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -67,9 +67,19 @@ class ViewController: UIViewController {
         guard textField.text?.isEmpty == false else { return }
         
         if let _ = Double(textField.text!) {
+            
+            let alert = UIAlertController(title: "Wrong format", message: "Pleas enter your name", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
             print("Name format is wrong")
+            
         } else {
-             label.text = textField.text
+            
+            label.text = textField.text
+            textField.text = nil
+            
         }
     
     }
