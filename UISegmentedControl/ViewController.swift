0c527fd8ec758e38c88fb28c6de1a12bc98f3089
Ruslan <ruslanfilistovich@gmail.com>
2019-08-11 10:39:22 +0300

    setting in viewController, add element label and button connect with code

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
-
+    @IBAction func choiseSegment(_ sender: UISegmentedControl) {
+        
+    }
+    
 
 }
 
